add04c56a248e55b3d55c0c1dd44d870d6e2ac97
Anjan Roy <anjanroy@yandex.com>
2021-08-04 15:57:11 +0530

    comment

--- systematic/decoder.go
+++ systematic/decoder.go
@@ -13,18 +13,31 @@ type SystematicRLNCDecoder struct {
 	rref                       matrix.Matrix
 }
 
+// Each piece of N-many bytes
 func (s *SystematicRLNCDecoder) PieceLength() uint {
 	return uint(len(s.pieces[0].Piece))
 }
 
+// Already decoded back to original pieces, with collected pieces ?
+//
+// If yes, no more pieces need to be collected
 func (s *SystematicRLNCDecoder) IsDecoded() bool {
 	return s.useful >= s.expected
 }
 
+// How many more pieces are required to be collected so that
+// whole data can be decoded successfully ?
+//
+// After collecting these many pieces, original data can be decoded
 func (s *SystematicRLNCDecoder) Required() uint {
 	return s.expected - s.useful
 }
 
+// Add one more collected coded piece, which will be used for decoding
+// back to original pieces
+//
+// If all required pieces are already collected i.e. successful decoding
+// has happened --- new pieces to be discarded, with an error denoting same
 func (s *SystematicRLNCDecoder) AddPiece(piece *kodr.CodedPiece) error {
 	s.pieces = append(s.pieces, piece)
 	s.received++
@@ -56,6 +69,8 @@ func (s *SystematicRLNCDecoder) AddPiece(piece *kodr.CodedPiece) error {
 	return nil
 }
 
+// After full decoding has happened, this method can be used for finding
+// i-th original piece, among N-pieces coded together --- so i < N, always
 func (s *SystematicRLNCDecoder) GetPiece(i uint) (kodr.Piece, error) {
 	if !s.IsDecoded() || i >= s.useful {
 		return nil, kodr.ErrMoreUsefulPiecesRequired
@@ -64,6 +79,7 @@ func (s *SystematicRLNCDecoder) GetPiece(i uint) (kodr.Piece, error) {
 	return s.rref[i][uint(len(s.rref[i]))-s.PieceLength():], nil
 }
 
+// All original pieces in order --- only when full decoding has happened
 func (s *SystematicRLNCDecoder) GetPieces() ([]kodr.Piece, error) {
 	if !s.IsDecoded() {
 		return nil, kodr.ErrMoreUsefulPiecesRequired
@@ -79,6 +95,15 @@ func (s *SystematicRLNCDecoder) GetPieces() ([]kodr.Piece, error) {
 	return pieces, nil
 }
 
+// Pieces coded by systematic mean, alogn with randomly coded pieces,
+// are decoded with this decoder
+//
+// @note Actually FullRLNCDecoder could have been used for same purpose
+// making this one redundant
+//
+// I'll consider improving decoding by exploiting
+// systematic coded pieces ( vectors )/ removing this
+// in some future date
 func NewSystematicRLNCDecoder(pieceCount uint) *SystematicRLNCDecoder {
 	return &SystematicRLNCDecoder{expected: pieceCount, field: galoisfield.DefaultGF256, pieces: make([]*kodr.CodedPiece, 0)}
 }
